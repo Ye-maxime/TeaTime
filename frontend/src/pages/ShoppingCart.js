12bb5018a385424ae872902dbf07d7879e34893e
zhengmao <yzmmaxime@163.com>
2019-06-21 14:44:30 +0200

    add shopping cart views

--- frontend/src/pages/ShoppingCart.js
+++ frontend/src/pages/ShoppingCart.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
+import connect from "react-redux/es/connect/connect";
+
+const Product = ({product}) => (
+    <div className="box drink-item level is-mobile">
+        <span>
+            <b>Product : </b> {product.name}
+            <b> Price :   </b> {product.price}
+        </span>
+        <span>
+            <b>Quantity : </b> {product.quantity}
+        </span>
+    </div>
+)
 
 class ShoppingCart extends Component {
     render() {
+        const {products, total} = this.props
         return (
             <div>
-                <h1>Page Shopping cart</h1>
+                <h1>Total : {total}</h1>
+                <div className="container drink-list">
+                    {products.map((product) =>
+                        <Product
+                            key={product.id}
+                            id={product.id}
+                            product={product}
+                        />)}
+                </div>
             </div>
         );
     }
 }
 
-export default ShoppingCart;
+const mapStateToProps = (state) => {
+    return {
+        products: state.shoppingCart.items,
+        total: state.shoppingCart.total
+    }
+}
+
+export default connect(mapStateToProps, null)(ShoppingCart)
--- frontend/src/reducers/reducerShoppingCart.js
+++ frontend/src/reducers/reducerShoppingCart.js
@@ -1,13 +1,28 @@
 import {ADD_TO_SHOPPING_CART} from "../actions/shoppingCart";
 
 export const SHOPPING_CART_DEFAULT_STATE = {
-    items: []
+    items: [],
+    total: 0
 }
 
 export default function reducerShoppingCart(state = SHOPPING_CART_DEFAULT_STATE, action) {
     switch (action.type) {
-        case ADD_TO_SHOPPING_CART:
+        case ADD_TO_SHOPPING_CART: {
             console.log("reducers addToShoppingCart !!!!")
-            return {items: state.items.concat(action.product)}
+            let newProduct = action.product
+            const existedItem = state.items.find(item => item.id === newProduct.id)
+            const newTotal = state.total + parseInt(newProduct.price)
+            if (existedItem) {
+                existedItem.quantity += 1
+                return {...state, total: newTotal}
+            } else {
+                newProduct.quantity = 1
+                return {...state, items: state.items.concat(newProduct), total: newTotal}
+            }
+        }
+
+        default:
+            console.log("reducerShoppingCart default !!!!")
+            return state
     }
 }
