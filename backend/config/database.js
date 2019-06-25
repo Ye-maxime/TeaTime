4a646e7a6e9304fce9659023f3159c4cebe1cd70
ZhengmaoYe <yzmmaxime@163.com>
2019-06-25 14:32:23 +0200

    first implementation sequelize

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const config = require('./config');
+
+module.exports =  new Sequelize(config.database, config.username, config.password, {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
