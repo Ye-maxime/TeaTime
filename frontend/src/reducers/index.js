b30e90eb99a05de0c1d6ef57fb129c0e06d28e06
XIE <xieyihan92@gmail.com>
2019-06-27 15:37:39 +0200

    add change

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -7,16 +7,15 @@ import reducerMenu,{MENUS_DEFAULT_STATE} from "./reducerMenu";
 const rootReducer = combineReducers({
     drinks: reducerDrinks,
     shoppingCart: reducerShoppingCart,
-    menus:reducerMenu
+    menus:reducerMenu,
     stores: reducerStores,
 })
 
 export const DEFAULT_STATE = {
     drinks: DRINKS_DEFAULT_STATE,
     shoppingCart: SHOPPING_CART_DEFAULT_STATE,
-    stores: STORES_DEFAULT_STATE,
-    menus:MENUS_DEFAULT_STATE
-    stores: STORES_DEFAULT_STATE,
+    menus:MENUS_DEFAULT_STATE,
+    stores: STORES_DEFAULT_STATE
 }
 
 export default rootReducer
