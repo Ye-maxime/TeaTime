77efd4f064cc341fa6daa64807477879abd6629e
XIE <xieyihan92@gmail.com>
2019-07-10 16:42:35 +0200

    add test for page account

--- frontend/src/test/Page.test.js
+++ frontend/src/test/Page.test.js
@@ -1,15 +1,23 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {shallow, mount} from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 import {Account} from "../pages/bundle";
+
 
 Enzyme.configure({adapter: new Adapter()});
 
 describe("Account page", () => {
-    it("renders", () => {
+    test("renders", () => {
         const wrapper = shallow(<Account/>);
         expect(wrapper.exists()).toBe(true)
     });
+
+
+    test("click left side bar is echoed", ()=> {
+        const wrapper = mount(<Account/>);
+        wrapper.find('LeftSideBar').find('TabItem').last().simulate('click');
+        expect(wrapper.state().tabSelectedId).toBe(4);
+    })
 });
 
 // describe("Menu page", () => {
