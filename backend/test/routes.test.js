8ba009b50e6ac254de2079bc3a3c7972cd84f3ec
Maxime <yzmmaxime@163.com>
2019-07-10 15:59:48 +0200

    add test for controllers

--- backend/test/routes.test.js
+++ backend/test/routes.test.js
@@ -3,8 +3,10 @@ const request = require("supertest")
 const database = require('../config/database')
 const {initStore, initDrink} = require('../models/index')
 
-// close the server after each test
-afterEach(() => {
+let socket
+
+// close the server after all test
+afterAll(() => {
   server.close();
 })
 
@@ -23,22 +25,41 @@ describe("Routes: index", () => {
   })
 
 
-  it("should get all drinks", async () => {
+  it("should get drinks", async () => {
     const response = await request(server).get("/v1/drinks");
-    expect(response.status).toBe(200);
-    expect(response.type).toBe("application/json");
     expect(response.body.length).toBe(6);
   });
 
-  it("should get all stores", async () => {
+  it("should create a drink", async () => {
+    const newDrink = {name: 'milk shake'}
+    const response = await request(server).post("/v1/drinks").send(newDrink);
+    expect(response.body.name).toBe('milk shake');
+  });
+
+  it("should get stores", async () => {
     const response = await request(server).get("/v1/stores");
-    expect(response.status).toBe(200);
-    expect(response.type).toBe("application/json");
-    console.log(response.body)
-    console.log("!!!!!!!!!")
     expect(response.body.length).toBe(2);
     expect(response.body[0].name).toBe('Teatime Opéra');
     expect(response.body[1].name).toBe('Teatime Haussmann');
+  });
+
+  it("should create, find and get orders", async () => {
+    const newOrder = {
+      products: [{
+        id: 1,
+        name: "Brown Sugar Deerioca Fresh Milk",
+        price: "12",
+        collection: "BROWN",
+        quantity: 1
+      }],
+      total: 12
+    }
+    await request(server).post("/v1/orders").send(newOrder);
+    const response = await request(server).get("/v1/orders");
+    expect(response.body.length).toBe(1);
+    const response2 = await request(server).post("/v1/order_detail/1");
+    expect(response2.body.length).toBe(1);
+    expect(response2.body[0].quantity).toBe(1);
   });
 
 });
