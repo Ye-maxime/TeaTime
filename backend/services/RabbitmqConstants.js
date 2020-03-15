76dde75a271ee707aef1ab3e0656edcfcf711d0f
maxime <yzmmaxime@163.com>
2020-03-15 20:50:05 +0100

    success to call mq

--- backend/services/RabbitmqConstants.js
+++ backend/services/RabbitmqConstants.js
@@ -1,5 +1,5 @@
 module.exports = {
     CONN_URL: 'amqp://localhost',
-    // CONN_URL: 'amqp://guest:guest@localhost',
+    // CONN_URL: 'amqp://rabbitmq:5672', // 启动docker时使用  //TODO use process.env to replace it 
     QUEUE_NAME: 'teatime placed order'
 }
